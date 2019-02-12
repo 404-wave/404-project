6c94ed30b222b91d521d87125b3f06ffb768e02e
Z-Red <araien@ualberta.ca>
2019-02-12 13:52:42 -0700

    Users can no longer login with 'inactive' accounts

--- Wave/users/models.py
+++ Wave/users/models.py
@@ -7,6 +7,8 @@ class User(AbstractUser):
 
     bio = models.TextField(max_length=500, blank=True)
     github = models.TextField(max_length=500, blank=True)
+    is_active = models.BooleanField(('active'), default=False)
+    is_admin = models.BooleanField(('admin'), default=False)
 
     def __str__(self):
-        return self.first_name + ' ' + self.last_name
+        return self.username
