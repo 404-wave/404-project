5b9c2f72514a30c3943324ef751d83280b26c093
Z-Red <araien@ualberta.ca>
2019-02-12 13:18:50 -0700

    Added comments

--- Wave/core/views.py
+++ Wave/core/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
 
 def home(request):
+    # TODO: If the user is not authenticated then don't show the home page,
+    # but instead show soe other page reporting the error. (Maybe just the login page).
     return render(request, 'home.html')
