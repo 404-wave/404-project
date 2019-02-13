851835c3938b75ca57128c6aabaa46e3cdd1a977
Z-Red <araien@ualberta.ca>
2019-02-12 22:22:56 -0700

    Remove administration app

--- Wave/Wave/urls.py
+++ Wave/Wave/urls.py
@@ -19,6 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path('', include('users.urls')),
     path('home/', include('core.urls')),
-    path('administration/', include('administration.urls')),
     path('admin/', admin.site.urls),
 ]
