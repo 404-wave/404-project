8e507c03020488b20df6c57d850fa6345e90849c
sewingpillows <allison.boukall@gmail.com>
2019-02-14 21:48:19 -0700

    start initial styling of registration page

--- Wave/users/forms.py
+++ Wave/users/forms.py
@@ -4,12 +4,11 @@ from .models import User
 
 class UserCreationForm(UserCreationForm):
 
-    first_name = forms.CharField(max_length=User._meta.get_field("first_name").max_length)
-    last_name = forms.CharField(max_length=User._meta.get_field("last_name").max_length)
+    first_name = forms.CharField(max_length=User._meta.get_field("first_name").max_length, required=False)
+    last_name = forms.CharField(max_length=User._meta.get_field("last_name").max_length, required=False)
     email = forms.CharField(max_length=User._meta.get_field("email").max_length)
-    github = forms.CharField(max_length=User._meta.get_field("github").max_length)
-    bio = forms.CharField(max_length=User._meta.get_field("bio").max_length)
-
+    github = forms.CharField(max_length=User._meta.get_field("github").max_length, required=False)
+    bio = forms.CharField(max_length=User._meta.get_field("bio").max_length,  required=False)
     class Meta(UserCreationForm):
         model = User
-        fields = ("username", "first_name", "last_name", "email", "github", "bio")
+        fields = ("username", "first_name", "last_name", "email", "github")
