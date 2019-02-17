5925f22219a23c962cc4a9bca1b6789997f63abe
sewingpillows <allison.boukall@gmail.com>
2019-02-17 14:38:55 -0700

    Customize help messages - better grammar

--- Wave/users/forms.py
+++ Wave/users/forms.py
@@ -3,12 +3,17 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import User
 
 class UserCreationForm(UserCreationForm):
-
     first_name = forms.CharField(max_length=User._meta.get_field("first_name").max_length, required=False)
     last_name = forms.CharField(max_length=User._meta.get_field("last_name").max_length, required=False)
-    email = forms.CharField(max_length=User._meta.get_field("email").max_length)
-    github = forms.CharField(max_length=User._meta.get_field("github").max_length, required=False)
-    bio = forms.CharField(max_length=User._meta.get_field("bio").max_length,  required=False)
+    email = forms.EmailField(max_length=User._meta.get_field("email").max_length)
+
+    def __init__(self, *args, **kwargs):
+        super(UserCreationForm, self).__init__(*args, **kwargs)
+        self.fields['password1'].help_text = "Make sure it's more than 30 characters and includes a number and a lowercase letter."
+        self.fields['password2'].help_text = "Enter the same password as above."
+        self.fields['username'].help_text = None
+
     class Meta(UserCreationForm):
         model = User
-        fields = ("username", "first_name", "last_name", "email", "github")
+        fields = ("username", "first_name", "last_name", "email", "password1")
+        field_order = ["username", "first_name", "last_name", "email"]
