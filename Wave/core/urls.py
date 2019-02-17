03ef6f9eaff085a1ccbb828c372ce551ec16179b
Z-Red <araien@ualberta.ca>
2019-02-17 11:10:59 -0700

    Can now go to specific user profiles

--- Wave/core/urls.py
+++ Wave/core/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('profile/', views.profile, name='profile')
+    path('profile/', views.profile, name='profile'),
+    path('profile/<int:pk>', views.profile, name='profile'), # TODO: Change to <uuid:pk> when implemented
 ]
