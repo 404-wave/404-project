451290f5611c0e8de610a6f250bd771065ffb126
pennyfea <pennyfea@ualberta.ca>
2019-02-16 20:35:28 -0700

    Added PostModelAdmin

--- Wave/posts/admin.py
+++ Wave/posts/admin.py
@@ -1,8 +1,22 @@
 from django.contrib import admin
 
 # Register your models here.
-
-
 from .models import Post
 
-admin.site.register(Post)
+# NOTE: It IS possible to have links in the admin interface, so later on we
+# will be able to have anchor tag links to images related to posts.
+
+# https://docs.djangoproject.com/en/2.1/ref/contrib/admin/
+
+
+class PostModelAdmin(admin.ModelAdmin):
+    list_display = ["id", "user", "timestamp", "updated"]
+    list_display_links = ["id", "user", "updated"]
+    list_filter = ["updated", "timestamp"]
+    search_fields = ["content", "user__username"]
+
+    class Meta:
+        model = Post
+
+
+admin.site.register(Post, PostModelAdmin)
