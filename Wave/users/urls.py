4a2a44209a903f1cc8d0a4a8c80206588c340581
Pierre Lin <pslin@ualberta.ca>
2019-02-18 22:48:58 -0500

    Github integration prototype

--- Wave/users/urls.py
+++ Wave/users/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('register/', views.register, name='register'),
+    path('post_repos/', views.post_repos, name='post_repos'),
+    path('list_events/', views.list_events, name='list_events'),
+    path('event_details/<str:event_type>/', views.event_details, name='event_details')
 ]
--- Wave/users/views.py
+++ Wave/users/views.py
@@ -4,6 +4,7 @@ from django.template import RequestContext
 from django.urls import reverse
 
 from .forms import UserCreationForm
+import requests
 
 
 def index(request):
@@ -29,3 +30,62 @@ def register(request):
         form = UserCreationForm()
         context = {'form' : form}
         return render(request, 'registration/register.html', context)
+
+def list_events(request):
+    if not request.user.is_authenticated:
+        return HttpResponseForbidden()
+
+    else:
+        #Should we worry about input sanitation here?
+        #print(request.user.github)
+        build_request = 'https://api.github.com/users/' + request.user.github + '/events'
+        r=requests.get(build_request)
+        response = r.json()
+        #print(response)
+        event_types =[]
+        for item in response:
+            if item['type'] not in event_types:
+                event_types.append(item['type'])
+            #repos.append(item['name'])
+        #print(repos)
+        #print(response)
+        #print(response)
+
+        return render(request, 'home.html', {'event_types': event_types})
+
+def event_details(request, event_type):
+    if not request.user.is_authenticated:
+        return HttpResponseForbidden()
+
+    else:
+        build_request = 'https://api.github.com/users/' + request.user.github + '/events'
+        r=requests.get(build_request)
+        response = r.json()
+        #print(response)
+        events =[]
+        for item in response:
+            if item['type'] == event_type:
+                events.append(item['repo']['url'])
+
+        return render(request, 'home.html', {'events': events})
+
+
+def post_repos(request):
+    if not request.user.is_authenticated:
+        return HttpResponseForbidden()
+
+    else:
+        #Should we worry about input sanitation here?
+        #print(request.user.github)
+        build_request = 'https://api.github.com/users/' + request.user.github + '/repos'
+        r=requests.get(build_request)
+        response = r.json()
+        #print(response)
+        repos =[]
+        for item in response:
+            repos.append(item['name'])
+        #print(repos)
+        #print(response)
+        #print(response)
+
+        return render(request, 'home.html', {'repos': repos, 'github': request.user.github})
