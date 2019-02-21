53a45bb058dc5f0f3e466d23ee94808210e34642
sewingpillows <allison.boukall@gmail.com>
2019-02-20 21:09:52 -0700

    Profile stylings

--- Wave/static/js/validation.js
+++ Wave/static/js/validation.js
@@ -5,3 +5,29 @@ function redAlert(errorField) {
     el = document.getElementById("id_"+errorField).parentElement
     el.classList.add("redAlert");
 }  
+function showTab(){
+    el = document.getElementById("content").children;
+    tabs = document.getElementById("tabs").children;
+    for (var i = 0; i < el.length; i++) {
+        if (this == tabs[i]){
+            el[i].setAttribute("id", "current");  
+            tabs[i].setAttribute("id", "current_tab")
+        }
+        else {
+            el[i].setAttribute("id", "hide_div");  
+            tabs[i].setAttribute("id", "hide_tab")
+        }
+    }
+}
+function myFunction() {
+    el = document.getElementById("content").children;
+    tabs = document.getElementById("tabs").children;
+    for (var i = 0; i < el.length; i++) {
+        el[i].setAttribute("id", "hide_div");  
+        tabs[0].setAttribute("id", "hide_tab")
+        tabs[i].addEventListener("click", showTab);
+    }
+    el[0].setAttribute("id", "current");
+    tabs[0].setAttribute("id", "current_tab")
+}
+window.onload = myFunction;
