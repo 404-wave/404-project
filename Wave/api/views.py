feaa0a43b120c98ed0fc9fd2a9f3cb624602829b
Z-Red <araien@ualberta.ca>
2019-03-11 22:44:12 -0600

    Set up POST, PUT methods for views

--- Wave/api/views.py
+++ Wave/api/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 
-from rest_framework import pagination, generics, views, status
+from rest_framework import pagination, generics, views, status, mixins
 from rest_framework.response import Response
+
+from rest_framework.views import APIView
 
 # Investigate permissons
 #from rest_framework.permissions import IsAdminUser
@@ -21,8 +23,8 @@ class UserAPIView(generics.GenericAPIView):
 
     def get(self, request, *args, **kwargs):
 
-        if not request.user.is_authenticated:
-            return Response(status=status.HTTP_401_UNAUTHORIZED)
+        # if not request.user.is_authenticated:
+        #     return Response(status=status.HTTP_401_UNAUTHORIZED)
 
         # Handle GETs for multiple author profiles, or a single profile
         if 'author_id' in kwargs.keys():
@@ -47,142 +49,153 @@ class UserAPIView(generics.GenericAPIView):
 
 class PostAPIView(generics.GenericAPIView):
 
-        queryset = Post.objects.all()
-        serializer_class = PostSerializer
-        pagination_class = PostPagination
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    pagination_class = PostPagination
 
-        def get(self, request, *args, **kwargs):
+    def get(self, request, *args, **kwargs):
 
-            if not request.user.is_authenticated:
-                return Response(status=status.HTTP_401_UNAUTHORIZED)
+        # if not request.user.is_authenticated:
+        #     return Response(status=status.HTTP_401_UNAUTHORIZED)
 
-            # Handle GETs for all public posts, or just a single post
-            if 'author_id' in self.kwargs.keys():
-                author_id = self.kwargs['author_id']
-                # TODO: Hanlde the privacy concerns of the posts w.r.t authenticated user of API
-                queryset = Post.objects.filter(user=author_id)
+        # Handle GETs for all public posts, or just a single post
+        if 'author_id' in self.kwargs.keys():
+            author_id = self.kwargs['author_id']
+            # TODO: Hanlde the privacy concerns of the posts w.r.t authenticated user of API
+            queryset = Post.objects.filter(user=author_id)
 
-            elif 'post_id' in kwargs.keys():
-                post_id = self.kwargs['post_id']
-                # TODO: Hanlde the privacy concerns of the posts w.r.t authenticated user of API
-                queryset = Post.objects.filter(id=post_id)
+        elif 'post_id' in kwargs.keys():
+            post_id = self.kwargs['post_id']
+            # TODO: Hanlde the privacy concerns of the posts w.r.t authenticated user of API
+            queryset = Post.objects.filter(id=post_id)
 
-            else:
-                queryset = Post.objects.filter(privacy=Post.PUBLIC)
+        else:
+            queryset = Post.objects.filter(privacy=Post.PUBLIC)
 
-            page = self.paginate_queryset(queryset)
-            if page is not None:
-                serializer = self.get_serializer(page, many=True)
-                return self.get_paginated_response(serializer.data)
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
 
-            serializer = PostSerializer(queryset, many=True)
-            return Response(serializer.data)
+        serializer = PostSerializer(queryset, many=True)
+        return Response(serializer.data)
 
+    def post(self, request, *args, **kwargs):
+        return Response(status=status.HTTP_501_NOT_IMPLEMENTED)
+
+    def put(self, request, *args, **kwargs):
+        return Response(status=status.HTTP_501_NOT_IMPLEMENTED)
 
 class CommentAPIView(generics.GenericAPIView):
 
-        queryset = Comment.objects.all()
-        serializer_class = CommentSerializer
-        pagination_class = CommentPagination
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    pagination_class = CommentPagination
 
-        def get(self, request, *args, **kwargs):
+    def get(self, request, *args, **kwargs):
 
-            if not request.user.is_authenticated:
-                return Response(status=status.HTTP_401_UNAUTHORIZED)
+        # if not request.user.is_authenticated:
+        #     return Response(status=status.HTTP_401_UNAUTHORIZED)
 
-            # Handle GETs for all comments belonging to a post
-            if 'post_id' in self.kwargs.keys():
-                post_id = self.kwargs['post_id']
-                # TODO: Hanlde the privacy concerns of the post w.r.t authenticated user of API?
-                # E.g., privacy inheritance
+        # Handle GETs for all comments belonging to a post
+        if 'post_id' in self.kwargs.keys():
+            post_id = self.kwargs['post_id']
+            # TODO: Hanlde the privacy concerns of the post w.r.t authenticated user of API?
+            # E.g., privacy inheritance
 
-                try:
-                    queryset = Post.objects.filter(id=post_id)[0].comments
-                except:
-                    Response(status=status.HTTP_404_NOT_FOUND)
+            try:
+                queryset = Post.objects.filter(id=post_id)[0].comments
+            except:
+                Response(status=status.HTTP_404_NOT_FOUND)
 
-            page = self.paginate_queryset(queryset)
-            if page is not None:
-                serializer = self.get_serializer(page, many=True)
-                return self.get_paginated_response(serializer.data)
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
 
-            serializer = CommentSerializer(queryset, many=True)
-            return Response(serializer.data)
+        serializer = CommentSerializer(queryset, many=True)
+        return Response(serializer.data)
 
+    def post(self, request, *args, **kwargs):
+        return Response(status=status.HTTP_501_NOT_IMPLEMENTED)
 
+#class FriendAPIView(generics.GenericAPIView, mixins.CreateModelMixin):
 class FriendAPIView(generics.GenericAPIView):
 
-        queryset = Comment.objects.all()
-        serializer_class = CommentSerializer
-        pagination_class = CommentPagination
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    pagination_class = CommentPagination
 
-        def get(self, request, *args, **kwargs):
+    def get(self, request, *args, **kwargs):
 
-            if not request.user.is_authenticated:
-                return Response(status=status.HTTP_401_UNAUTHORIZED)
+        # if not request.user.is_authenticated:
+        #     return Response(status=status.HTTP_401_UNAUTHORIZED)
 
-            # GET the friends of a user
-            if 'author_id' in self.kwargs.keys():
-                author_id = self.kwargs['author_id']
+        # GET the friends of a user
+        if 'author_id' in self.kwargs.keys():
+            author_id = self.kwargs['author_id']
 
-                # Build queryset for the friends of the specified author
-                friends = ""
-                try:
-                    followers = User.objects.filter(follower__user2=author_id, is_active=True)
-                    following = User.objects.filter(followee__user1=author_id, is_active=True)
-                    friends = following & followers
-                except:
-                    return Response(status=status.HTTP_404_NOT_FOUND)
-
-                # Iterate over the queryset to find the IDs of the friends
-                friend_list = list()
-                for friend in friends:
-                    friend_list.append(str(friend.id))
-
-                return Response({"query": "friends", "authors": friend_list})
-
-            # GET to see if two users are friends
-            if 'author_id1' in self.kwargs.keys() and 'author_id2' in self.kwargs.keys():
-
-                # Build queryset for the friends of the specified author
-                author_id1 = self.kwargs['author_id1']
-                author_id2 = self.kwargs['author_id2']
-
-                # Get a friend list for one of the users
-                friend_list = ""
-                try:
-                    followers = User.objects.filter(follower__user2=author_id1, is_active=True)
-                    following = User.objects.filter(followee__user1=author_id1, is_active=True)
-                    friend_list = following & followers
-                except:
-                    return Response(status=status.HTTP_404_NOT_FOUND)
-
-                # Iterate over the friend list to check for friendship
-                friends = False
-                for friend in friend_list:
-                    if friend.id == author_id2:
-                        friends = True
-                        break
-
-                # Whether there is friendship or not, we need some author data
-                author1 = User
-                author2 = User
-                try:
-                    author1 = User.objects.get(id=author_id1)
-                    author2 = User.objects.get(id=author_id2)
-                except:
-                    return Response(status=status.HTTP_404_NOT_FOUND)
-
-                # Format the response
-                response = {
-                    "query":"friends",
-                    "authors":[
-                        str(author1.host) + str(author1.id),
-                        str(author2.host) + str(author2.id)
-                    ],
-                    "friends": friends
-                }
-
-                return Response(response)
-            else:
+            # Build queryset for the friends of the specified author
+            friends = ""
+            try:
+                followers = User.objects.filter(follower__user2=author_id, is_active=True)
+                following = User.objects.filter(followee__user1=author_id, is_active=True)
+                friends = following & followers
+            except:
                 return Response(status=status.HTTP_404_NOT_FOUND)
+
+            # Iterate over the queryset to find the IDs of the friends
+            friend_list = list()
+            for friend in friends:
+                friend_list.append(str(friend.id))
+
+            return Response({"query": "friends", "authors": friend_list})
+
+        # GET to see if two users are friends
+        if 'author_id1' in self.kwargs.keys() and 'author_id2' in self.kwargs.keys():
+
+            # Build queryset for the friends of the specified author
+            author_id1 = self.kwargs['author_id1']
+            author_id2 = self.kwargs['author_id2']
+
+            # Get a friend list for one of the users
+            friend_list = ""
+            try:
+                followers = User.objects.filter(follower__user2=author_id1, is_active=True)
+                following = User.objects.filter(followee__user1=author_id1, is_active=True)
+                friend_list = following & followers
+            except:
+                return Response(status=status.HTTP_404_NOT_FOUND)
+
+            # Iterate over the friend list to check for friendship
+            friends = False
+            for friend in friend_list:
+                if friend.id == author_id2:
+                    friends = True
+                    break
+
+            # Whether there is friendship or not, we need some author data
+            author1 = User
+            author2 = User
+            try:
+                author1 = User.objects.get(id=author_id1)
+                author2 = User.objects.get(id=author_id2)
+            except:
+                return Response(status=status.HTTP_404_NOT_FOUND)
+
+            # Format the response
+            response = {
+                "query":"friends",
+                "authors":[
+                    str(author1.host) + str(author1.id),
+                    str(author2.host) + str(author2.id)
+                ],
+                "friends": friends
+            }
+
+            return Response(response)
+        else:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+    def post(self, request, *args, **kwargs):
+        return Response(status=status.HTTP_501_NOT_IMPLEMENTED)
