31e71a819f3d889ecb795630cad303cceca0ab23
sewingpillows <allison.boukall@gmail.com>
2019-03-12 20:21:02 -0600

    Ensure style changes or class does not affect follow/unfollow button

--- Wave/core/views.py
+++ Wave/core/views.py
@@ -219,11 +219,14 @@ def profile(request, pk = None):
 
 	# Check if we follow the user whose profile we are looking at
 	following = False
+	button_text = "Unfollow"
 	if request.user.id is not pk:
 		following = follows(request.user.id, pk)
+		if (following == False):
+			button_text = "Follow"
 
 
-	return render(request, 'profile.html', {'user': user, 'following': following})
+	return render(request, 'profile.html', {'user': user, 'following': following, 'button_text': button_text})
 
 
 
