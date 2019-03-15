158c8e3fb736171818d4b3cde4760bcb3c2fa541
pennyfea <pennyfea@ualberta.ca>
2019-03-14 21:02:04 -0600

    Added import django_heroku

--- Wave/settings.py
+++ Wave/settings.py
@@ -8,10 +8,13 @@ https://docs.djangoproject.com/en/2.1/topics/settings/
 
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
+
 """
+
 
 import os
 import django_heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -143,4 +146,5 @@ STATICFILES_DIRS = [
 ]
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media_cdn")
+
 django_heroku.settings(locals())
