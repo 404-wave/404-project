cc098e6a59520cc3e953e22c70e147d22d001b4c
pennyfea <pennyfea@ualberta.ca>
2019-03-14 20:05:16 -0600

    Delete migrations for fresh database

--- Wave/settings.py
+++ Wave/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -141,3 +142,4 @@ STATICFILES_DIRS = [
 ]
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media_cdn")
+django_heroku.settings(locals())
