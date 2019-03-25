0841d682021840ffb0f31cf55326b45daf65ecff
sewingpillows <allison.boukall@gmail.com>
2019-03-25 14:51:28 -0600

    figure out condition

--- posts/templatetags/post_filters.py
+++ posts/templatetags/post_filters.py
@@ -5,6 +5,8 @@ register = template.Library()
 
 @register.filter(name='get_author_name')
 def get_author_name(value1):
-    if (value1.id):
+    if (isinstance(value1, dict)): 
         print (type(value1))
-        print ("HERE", value1.id)
+    else:
+        pass
+
