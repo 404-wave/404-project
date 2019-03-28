e907a1c85fff4465c836adbfcd2d7257b3a32b5d
kerryli33 <kerry2@ualberta.ca>
2019-03-27 19:23:52 -0600

    added django-cors middleware

--- Wave/settings.py
+++ Wave/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
 
     # local apps
     'rest_framework',
@@ -58,6 +59,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -154,3 +157,5 @@ MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media_cdn")
 
 django_heroku.settings(locals())
+
+CORS_ORIGIN_ALLOW_ALL = True
