a921822bc3277ad0267da91c200c2cc46521d0a5
kerryli33 <kerry2@ualberta.ca>
2019-03-27 21:00:49 -0600

    debug

--- static/js/friends.js
+++ static/js/friends.js
@@ -210,7 +210,10 @@ function addFromOtherNode(data){
   // }
   $.ajax({
     url:path,
+    type:"POST",
     body:JSON.stringify(payload),
+    dataType: "json",
+    contentType: "application/json",
     success: function(){
       console.log("Successfully sent Request to Other Server");
     },
