62e841ec0ecbe15b09654d3b1fa7276b904f0516
kerryli33 <kerry2@ualberta.ca>
2019-03-29 11:53:45 -0600

    deleted restrictins on headers

--- Wave/settings.py
+++ Wave/settings.py
@@ -163,17 +163,6 @@ django_heroku.settings(locals())
 APPEND_SLASH = True
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
-CORS_ALLOW_HEADERS = (
-    'accept',
-    'accept-encoding',
-    'authorization',
-    'content-type',
-    'dnt',
-    'origin',
-    'user-agent',
-    'x-csrftoken',
-    'x-requested-with',
-)
 
 
 django_heroku.settings(locals())
