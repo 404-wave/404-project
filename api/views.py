828974bea67cb289e6f32026e62353ea3358fc7a
kerryli33 <kerry2@ualberta.ca>
2019-04-01 16:20:40 -0600

    debug

--- api/views.py
+++ api/views.py
@@ -466,7 +466,8 @@ class FriendAPIView(generics.GenericAPIView):
 
             author_id1 = None
             author_id2 = None
-            author1_server = standardize_url(NodeSetting.objects.all().get())
+            author1_server = NodeSetting.objects.all().get()
+            author1_server = standardize_url(author1_server.host)
             author2_server = None
             try:
                 author_id1 = self.kwargs['author_id1']
