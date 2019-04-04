29dc80cd6c7ed43c6a72e6d4bae35dd7c73f7c1c
sewingpillows <allison.boukall@gmail.com>
2019-04-03 20:56:03 -0600

    add extra date format catch

--- posts/models.py
+++ posts/models.py
@@ -50,14 +50,13 @@ class PostManager(models.Manager):
     """
         Functions that were made to test individual privacy setting
     """
-    def convert_to_date(self,elem):
-        
+    def convert_to_date(self,elem):   
         new_dt = re.sub(r'.[0-9]{2}:[0-9]{2}$','',elem['published'])
         try:
             new_dt = datetime.datetime.strptime(new_dt, '%Y-%m-%dT%H:%M:%S.%f') 
         except:
             try:
-                new_dt = datetime.datetime.strptime(new_dt, '%Y-%m-%dT%H:%M:%S.%f')
+                new_dt = datetime.datetime.strptime(new_dt, '%Y-%m-%dT%H:%M:%S')
             except:
                 return 'no date'
         return new_dt
