5cd699f5b3f50bed435d9d5318ac1eb761245009
pennyfea <pennyfea@ualberta.ca>
2019-04-06 23:58:58 -0600

    Not sure if i'm fixing this

--- posts/models.py
+++ posts/models.py
@@ -156,7 +156,6 @@ class PostManager(models.Manager):
 
                     #if servers are bad and don't include the author server we do
                     for item in responselist["posts"]:
-                        print("These are the duplicates from our server after a refresh", item['published']['id'])
                         if item['id'] in post_ids:
                             continue
                         post_ids.append(item['id'])
